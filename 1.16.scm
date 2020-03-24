935e52ed796a920aa2bbb2cbebdd2c3baa5f55de
codert <codert.sn@gmail.com>
2020-03-24 23:01:32 +0800

    chore: add 1.16 exercise

---
+++ 1.16.scm
@@ -0,0 +1,12 @@
+(define (exp base n)
+    (exp-iter 1 base n))
+
+(define (even? val)
+    (= (/ val 2) 0))
+
+(define (exp-iter product base n)
+    (if (= n 0)
+        product
+        (if (even? n)
+            (exp-iter product (square base) (/ n 2))
+            (exp-iter (* product base) base (- n 1)))))
