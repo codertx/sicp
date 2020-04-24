164a1aca1e6771d1ce6dac579a3d76c78cd0f401
codert <codert.sn@gmail.com>
2020-04-24 10:18:09 +0800

    chore: add some solution

---
+++ 2.30.scm
@@ -0,0 +1,11 @@
+(define (square-tree tree)
+    (cond ((null? tree) (list))
+          ((not (pair? tree)) (square tree))
+          (else (cons (square-tree (car tree))
+                      (square-tree (cdr tree))))))
+
+(define (square-tree-map tree)
+    (map (lambda (sub-tree)
+            (cond ((pair? sub-tree) (square-tree-map sub-tree))
+                  (else (square sub-tree))))
+        tree))
---
+++ 2.32.scm
@@ -0,0 +1,6 @@
+(define (subsets s)
+  (if (null? s)
+      (list (list))
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (subset)
+                            (cons (car s) subset)) rest)))))
